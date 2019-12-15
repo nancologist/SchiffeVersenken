65ebfa5cc11bba0dc2d38fcd23f0c7a69ce0cf01
Morteza Jalilifar <morteza.jalilifar@publicispixelpark.de>
2019-12-15 13:33:31 +0100

    modify font color and handle out of range exception

--- SchiffeVersenken/Player.cs
+++ SchiffeVersenken/Player.cs
@@ -19,14 +19,24 @@ namespace SchiffeVersenken
                 int x = -1;
                 int y = -1;
 
-                // Try&Catch: Damit wenn der User aus Versehen eine andere Taste wie "Enter" druckt, der Programm uns nicht rausschmeißt
+                // Try&Catch:
+                // Catch1: Wenn der User eine Zahl größer als neun eingibt...
+                // Catch2: Damit wenn der User aus Versehen eine andere Taste wie "Enter" druckt, der Programm uns nicht rausschmeißt
                 try
                 {
                     AskUserToShoot(x, y, bingo_0, board);
                 }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("The input number should be between 0 and 9 (inclusive)");
+                    AskUserToShoot(x, y, bingo_0, board);
+                }
                 catch (FormatException)
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.Write("Invalid input. Do you want to leave the game? (y/n): ");
+
                     string answer = Console.ReadLine().ToLower();
 
                     if (answer == "n")
@@ -52,6 +62,8 @@ namespace SchiffeVersenken
 
         public static void AskUserToShoot(int x, int y, int bingo_0, Board board)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+
             Console.Write("Enter x: ");
             x = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter y: ");
@@ -86,6 +98,7 @@ namespace SchiffeVersenken
             if (bingo == bingo_0)
             {
                 Board.InitBlankField(y, x, 2);
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine("Sorry, you did not shoot any ship, try again!");
                 Console.WriteLine($"Only {10 - bingo} more ships!");
             }
